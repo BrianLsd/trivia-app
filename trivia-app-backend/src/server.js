3895367486c80cb19492490ecafaed7de5480210
Zhe Li <zhe.li99@ed.cna.nl.ca>
2023-04-01 17:24:29 -0230

    Installed Mongo and Nodemon

--- trivia-app-backend/src/server.js
+++ trivia-app-backend/src/server.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import {MongoClient} from 'mongodb';
 
 const app = express();
 const port = 8000;
 
 app.listen(port, () => console.log(`Server is running on localhost:${port}`));
+
+app.get('/api/questions', async (req, res) => {
+    const client = new MongoClient(process.env.MONGO_CONNECT);
+    await client.connect();
+
+    const db = client.db('trivia');
+})
