95e2c5c88be081eb3e13ecdf2ec8e2861a9a150b
Zhe Li <zhe.li99@ed.cna.nl.ca>
2023-04-11 01:17:30 -0230

    Added an API route for adding additional data in MongoDB.

--- trivia-app-backend/src/server.js
+++ trivia-app-backend/src/server.js
@@ -14,6 +14,14 @@ const __dirname = path.dirname(__filename)
 const app = express();
 const port = 8000;
 
+const validateApiKey = (req, res, next) => { 
+    const apiKey = req.header('x-api-key');
+    if (apiKey !== process.env.API_KEY){
+        return res.status(401).json({message: 'Invalid API key'});
+    }
+    next(); // if the API key is OK, proceed
+}
+
 app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'))
 });
@@ -37,7 +45,7 @@ app.get('/api/questions', async (req, res) => { // tested with Postman
     res.json({data});
 });
 
-app.post('/api/admin/overwrite', jsonParser, async(req, res) => { // overwrite data
+app.post('/api/admin/overwrite', validateApiKey, jsonParser, async(req, res) => { // overwrite data
     const client = new MongoClient(process.env.MONGO_CONNECT);
     await client.connect();
 
@@ -45,11 +53,27 @@ app.post('/api/admin/overwrite', jsonParser, async(req, res) => { // overwrite d
     try {
         const deleteResult = await db.collection('questions').deleteMany({});
         const insertResult = await db.collection('questions').insertMany(req.body);
+        res.sendStatus(200);
     }
     catch (e) {
         console.log(e);
+        res.sendStatus(500);
     }
-    res.sendStatus(200);
+    
+})
+
+app.post('/api/admin/add', validateApiKey, jsonParser, async(req, res) => {
+    const client = new MongoClient(process.env.MONGO_CONNECT);
+    await client.connect();
+
+    const db = client.db('trivia');
+    try {
+        const insertResult = await db.collection('questions').insertMany(req.body);
+        res.sendStatus(200);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 })
 
 app.listen(port, () => console.log(`Server is running on localhost:${port}`));
